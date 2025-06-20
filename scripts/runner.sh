1d76a61e377ca8c2a37c73f50954a00af7adf460
Po Tato <SudeepBajpai@paypoint.com>
2025-06-20 13:35:03 +0100

    updated token

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -4,6 +4,18 @@
 
 set -euxo pipefail
 
+# Variables
+REPO_URL="https://github.com/oldmxnk/k8s-vagrant"
+API_URL="https://api.github.com/repos/oldmxnk/k8s-vagrant"
+
+GITHUB_TOKEN="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"
+
+RUNNER_NAME="self-hosted-k8s-runner"
+RUNNER_LABELS="self-hosted,Linux,K8s"
+
+# Install dependencies
+sudo apt-get install -y curl jq
+
 #
 # Install Runner
 #
@@ -11,18 +23,25 @@ set -euxo pipefail
 # Create a folder
 mkdir actions-runner 
 cd actions-runner
+
 # Download the latest runner package
 curl -o actions-runner-linux-x64-2.322.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.322.0/actions-runner-linux-x64-2.322.0.tar.gz
+
 # Extract the installer
 tar xzf ./actions-runner-linux-x64-2.322.0.tar.gz
 chown -R vagrant:vagrant /home/vagrant/actions-runner 
+rm -f ./actions-runner-linux-x64-2.322.0.tar.gz
+
+# Get the runner token from GitHub
+RUNNER_TOKEN=$(curl -X POST -H "Authorization: Bearer $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" "$API_URL/actions/runners/registration-token" | jq -e -r .token)
 
 #
 # Configure Runner
 #
 
 # Create the runner and start the configuration experience
-sudo -u vagrant ./config.sh --unattended --replace --url https://github.com/oldmxnk/k8s-vagrant --token BGB5O5SVAVF2LQ6U26V3FQLHY5JV2
+sudo -u vagrant ./config.sh --unattended --replace --url $REPO_URL --token $RUNNER_TOKEN --name $RUNNER_NAME --labels $RUNNER_LABELS
+
 # Last step, install service and start it
 sudo ./svc.sh install vagrant
 sudo ./svc.sh start
@@ -45,3 +64,13 @@ mkdir -p /home/vagrant/.kube
 sudo cp -i $config_path/config /home/vagrant/.kube/
 sudo chown 1000:1000 /home/vagrant/.kube/config
 EOF
+
+#
+# Install Helm
+#
+
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
